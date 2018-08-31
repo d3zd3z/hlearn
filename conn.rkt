9dfa16fb307d26a3481fcd0b896608cedc389263
David Brown <davidb@davidb.org>
2018-08-31 10:18:38 -0600

    Enhance database checking
    Make sure the schema version matches, and provide a function to retrieve
    the lesson kind.

--- conn.rkt
+++ conn.rkt
@@ -6,6 +6,7 @@
 
 (provide conn
          with-database
+         get-kind
          query1)
 
 ;;; The database connection.
@@ -19,9 +20,20 @@
     void
     (lambda ()
       (parameterize ([conn db])
+        (check-schema)
         (action)))
     (lambda ()
       (disconnect db))))
+
+;;; Check the schema version.
+(define (check-schema)
+  (define ver (query1 "SELECT * FROM schema_version"))
+  (unless (string=? ver "20170709A")
+    (error "Database schema version mismatch" ver "20170709A")))
+
+;;; Get the problem set kind.
+(define (get-kind)
+  (query1 "SELECT value FROM config WHERE key = 'kind'"))
 
 ;;; A query that expects a single row of a single value.
 (define (query1 query . args)
